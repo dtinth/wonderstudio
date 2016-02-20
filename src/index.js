6bedd06ca94098cf4cc9c2cf5bff167f5e208bd2
Thai Pangsakulyanont <dtinth@spacet.me>
2016-02-20 14:14:13 +0700

    critically cssy

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
+
+// Import React to use react-router
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
+
+// Material-UI prerequisites
 import * as colors from 'material-ui/lib/styles/colors'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
 
-import 'normalize.css'
-import './loading.styl'
-import styles from './index.styl'
+// Fonts
 import { loadCSS } from 'fg-loadcss'
-
-// Not sure why, but this fixes some weird bug where some modules cannot be loaded!
-require.include('./app/Components')
-
-document.body.className = styles.body
-
 loadCSS('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700')
 loadCSS('https://fonts.googleapis.com/css?family=Roboto+Mono:400,700')
 loadCSS('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,600')
+
+// Stylesheets
+import 'normalize.css'
+import './loading.styl'
+import './index.styl'
+
+// Not sure why, but this fixes some weird bug where some modules cannot be loaded!
+require.include('./app/Components')
 
 import reproxy from './reproxy'
 import StudioAppProxy from 'react-proxy-loader?name=StudioApp!./studio/StudioApp'
