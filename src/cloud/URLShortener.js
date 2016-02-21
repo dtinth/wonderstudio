13808ce0d808de5347e1d255e9136654741ed7a7
Thai Pangsakulyanont <dtinth@spacet.me>
2016-02-22 03:52:29 +0700

    Cache URL shortener result

--- src/cloud/URLShortener.js
+++ src/cloud/URLShortener.js
@@ -1,8 +1,10 @@
 
 const API_KEY = 'AIzaSyBXdSaZqAhTK0cgHSRR8fi0Hua5DBNOBvc'
+const cache = { }
 
 // Can’t get whatwg-fetch to work... Gotta go with plain ol XHR.
 export async function shorten (longUrl) {
+  if (cache[longUrl]) return cache[longUrl]
   const request = JSON.stringify({ longUrl })
   const xhr = new window.XMLHttpRequest()
   xhr.open('POST', 'https://www.googleapis.com/urlshortener/v1/url?key=' + API_KEY, true)
@@ -15,5 +17,6 @@ export async function shorten (longUrl) {
   const data = JSON.parse(result)
   const shortUrl = data.id
   if (!shortUrl) throw new Error('Google did not return a short URL')
+  cache[longUrl] = shortUrl
   return shortUrl
 }
